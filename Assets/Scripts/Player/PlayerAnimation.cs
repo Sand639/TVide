e898e7d782a08ebfa359b3e9e89ef43e86d64b37
Sand639 <yziku100@gmail.com>
2025-07-17 19:19:20 +0900

    テクスチャ変更
    歩きモーションと
    ジャンプモーションまで

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -11,14 +11,26 @@ public class PlayerAnimation : MonoBehaviour
     [Header("次のスプライトに移動するための遅延時間")]
     [SerializeField] private float delayTime = 0.1f;
 
+    private float currentDelayTime = 0.1f;
+
     //現在のスプライトに移動してから経過した時間
     private float timer = 0f;
     //現在のスプライトが左から何枚目か
     private int currentSprite = 0;
-    // 0:下　1:右　2:左 3:上    アニメーション順
+    // 0:右　1:左 2:下 3:上    アニメーション順
     private int oldDirection = 0;
 
+    //一連の動作のスプライト数
+    private int spriteAnimNum = 3;
 
+    //ジャンプアニメーションが終了したかどうか
+    private bool isJumpAnimEnd = false;
+
+    //ノックバックアニメーションが終了したかどうか
+    private bool isKnockbackAnimEnd = false;
+
+    //起き上がりアニメーションが開始されたかどうか
+    private bool isGetupAnimStart = false;
 
     private void Awake()
     {
@@ -29,7 +41,7 @@ public class PlayerAnimation : MonoBehaviour
 
     void Update()
     {
-        //一時停止スキル発動中は
+        //一時停止スキル発動中はアニメーションしない
         if (InventoryItemSpawn.IsFrameActive) return;
 
         //現在フレームの移動方向を保持
@@ -58,29 +70,43 @@ public class PlayerAnimation : MonoBehaviour
             currentDirection = oldDirection;
 
             //斜め方向を追加したら消す処理
-            if (currentDirection == -1) {
-                if (right) currentDirection = 1;    //右
-                else if (left) currentDirection = 2;//左
+            if (currentDirection == -1)
+            {
+                if (right) currentDirection = 0;    //右
+                else if (left) currentDirection = 1;//左
             }
 
+        }
 
-        }
-        //上下方向の移動
-        else if (vertical)
-        {
-            if (up) currentDirection = 3;       //上
-            else if (down) currentDirection = 0;//下
-        }
+        ////上下方向の移動
+        //else if (vertical)
+        //{
+        //    if (up) currentDirection = 3;       //上
+        //    else if (down) currentDirection = 2;//下
+        //}
         //左右方向の移動
         else if (horizontal)
         {
-            if (right) currentDirection = 1;    //右
-            else if (left) currentDirection = 2;//左
+            if (right) currentDirection = 0;    //右
+            else if (left) currentDirection = 1;//左
         }
+
+        spriteAnimNum = 3; //アニメーションのスプライト数を3に設定
+        currentDelayTime = delayTime;
+
+        //ジャンプ処理のアニメーション割込み
+        currentDirection = JumpAnim(currentDirection);
+
+        //ノックバックモーションの割り込み
+        //currentDirection = KnockbackAnim(currentDirection);
+
+        //起き上がりアニメーション
+        //currentDirection = GetupAnim(currentDirection);
+
 
         //プレイヤーが移動中のみアニメーションを再生
         //プレイヤーが移動しているまたは現在のスプライトが動作しているなら
-        if (currentDirection != -1 || (currentSprite != 0 && currentSprite != 3))
+        if (currentDirection != -1 || (currentSprite != 1 % 3))
         {
             //1フレーム前の移動方向と現在の移動方向が違う且つプレイヤーが移動している時
             if (currentDirection != oldDirection && currentDirection != -1)
@@ -92,16 +118,55 @@ public class PlayerAnimation : MonoBehaviour
             // 現在のスプライトに移動してから経過した時間を加算
             timer += Time.deltaTime;
             //delayTime秒過ぎたら次のスプライトに移動する
-            if (timer >= delayTime)
+            if (timer >= currentDelayTime)
             {
                 //timerをリセット
-                timer -= delayTime;
+                timer -= currentDelayTime;
                 //次のスプライトに移動させる
                 ++currentSprite;
-                //アニメーションが6枚で一周のため0～5の範囲で循環させる
-                currentSprite = currentSprite % 6;
+                //アニメーションがspriteAnimNumで一周のため0～(spriteAnimNum - 1)の範囲で循環させる
+                currentSprite = currentSprite % (spriteAnimNum - 1);
                 //移動方向によってスプライトが上から何番目かを掛けてあげる
-                spriteRenderer.sprite = sprites[oldDirection * 6 + currentSprite];
+                spriteRenderer.sprite = sprites[oldDirection * 5 + currentSprite];
+
+                if (currentSprite == (spriteAnimNum - 1))
+                {
+                    if (!PlayerMovement.Instance.isGrounded)
+                    {
+                        //ジャンプアニメーションが終了したらフラグを立てる
+                        isJumpAnimEnd = true;
+                    }
+
+                    if (isGetupAnimStart)
+                    {
+                        //起き上がりアニメーション終了フラグをリセット
+                        isGetupAnimStart = false;
+
+                        switch (currentDirection)
+                        {
+                            case 5:
+                                oldDirection = 0;
+                                break;
+                            case 7:
+                                oldDirection = 1;
+                                break;
+                        }
+                        currentSprite = 1;
+
+                    }
+
+                    if (PlayerMovement.Instance.isKnockback)
+                    {
+                        //ノックバックアニメーションが終了したらフラグを立てる
+                        isKnockbackAnimEnd = true;
+
+                        //ノックバックが終了したなら起き上がりアニメーション開始
+                        //起き上がりアニメーションを開始フラグを立てる
+                        isGetupAnimStart = true;
+                        currentSprite = 0; //起き上がりアニメーションのスプライトを0にリセット
+                    }
+
+                }
             }
         }
         else
@@ -109,6 +174,156 @@ public class PlayerAnimation : MonoBehaviour
             //プレイヤーが移動していないならtimerを0にする
             timer = 0f;
         }
+    }
+
+    //ジャンプアニメーションの処理
+    private int JumpAnim(int direction)
+    {
+        //ノックバック中ならジャンプアニメーションはしない
+        if (PlayerMovement.Instance.isKnockback) return direction;
+
+        //地面についていない時ジャンプモーション
+        if (!PlayerMovement.Instance.isGrounded)
+        {
+            //ジャンプアニメーションが終了したならアニメーション停止
+            if (isJumpAnimEnd) return -1;
+
+            //何も操作がないときに前フレームの方向を取得する
+            if (direction == -1)
+            {
+                //前フレームの方向を取得
+                direction = oldDirection;
+            }
+
+            spriteAnimNum = 5; //ジャンプのスプライト数を5に設定
+            switch (direction)
+            {
+                case 0: //右ジャンプ
+                    return 2;
+                case 1: //左ジャンプ
+                    return 3;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            //ジャンプアニメーション終了フラグをリセット
+            isJumpAnimEnd = false;
+        }
+
+        return direction;
+    }
+
+    //ノックバックアニメーションの処理
+    private int KnockbackAnim(int direction)
+    {
+        if (isGetupAnimStart) return direction;
+
+        //ノックバックモーションのアニメーション処理
+        if (PlayerMovement.Instance.isKnockback)
+        {
+
+            currentDelayTime = delayTime / 5.0f;
+
+
+            //何も操作がないときに前フレームの方向を取得する
+            if (direction == -1)
+            {
+                //前フレームの方向を取得
+                direction = oldDirection;
+            }
+
+            spriteAnimNum = 4; //ノックバックのスプライト数を4に設定
+            switch (direction)
+            {
+                case 0: //右ノックバック
+                    return 4;
+                case 1: //左ノックバック
+                    return 6;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            //ノックバックアニメーション終了フラグをリセット
+            isKnockbackAnimEnd = false;
+        }
+
+        return direction;
+    }
+
+    //起き上がりアニメーションの処理
+    private int GetupAnim(int direction)
+    {
+        //起き上がりアニメーションが開始されていないなら何もしない
+        if (isGetupAnimStart)
+        {
+            currentDelayTime = delayTime;
+
+            //何も操作がないときに前フレームの方向を取得する
+            if (direction == -1)
+            {
+                //前フレームの方向を取得
+                direction = oldDirection;
+            }
+            spriteAnimNum = 3; //起き上がりのスプライト数を3に設定
+            switch (direction)
+            {
+                case 0: //右起き上がり
+                case 2:
+                case 4:
+                    return 5;
+                case 1: //左起き上がり
+                case 3:
+                case 6:
+                    return 7;
+                case 5:
+                    break;
+                case 7:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        return direction;
+
     }
 
 }
